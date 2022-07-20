5a0e70d57755356baa6d2f2b71513862bfcdc804
Tkhan Vin <gatorfl2014@gmail.com>
2022-07-20 14:31:31 -0400

    add css

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
