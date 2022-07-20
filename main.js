c6dd1ffd955e9c0d90e1c5892821aaad3ee4fd67
Tkhan Vin <gatorfl2014@gmail.com>
2022-07-20 14:40:14 -0400

    add button

---
+++ main.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('button')
+
+const buttonAlert = () => {
+    alert('My button worked! Yay! Celebrate or something.')
+}
+
+button.addEventListener('click', buttonAlert)
--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '../styles.css'))
 })
 
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../main.js'))
+})
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
